ae67e3d87bec95d7f98587310edde14ca2a2d961
MaciejK98 <maciomacio110@gmail.com>
2021-09-06 00:57:29 +0200

    added donuts

--- src/index.ts
+++ src/index.ts
@@ -50,9 +50,21 @@ const brickWallbasecolor = textureLoader.load("./Wood_Ceiling_Coffers_002_baseco
 const brickWallnormalMap = textureLoader.load("./Wood_Ceiling_Coffers_002_normal.jpg");
 const brickWallheightMap = textureLoader.load("./Wood_Ceiling_Coffers_002_height.png");
 
-const stylizedBirckbasecolor = textureLoader.load("./Blue_Ice_001_COLOR.jpg");
-const stylizedBircknormalMap = textureLoader.load("./Blue_Ice_001_NORM.jpg");
-const stylizedBirckheightMap = textureLoader.load("./Blue_Ice_001_DISP.png");
+const iceBase = textureLoader.load("./Blue_Ice_001_COLOR.jpg");
+const iceNormal = textureLoader.load("./Blue_Ice_001_NORM.jpg");
+const iceHeight = textureLoader.load("./Blue_Ice_001_DISP.png");
+iceBase.wrapS= THREE.RepeatWrapping;
+iceBase.wrapT= THREE.RepeatWrapping;
+iceBase.repeat.x = 3;
+iceBase.repeat.y = 2; 
+iceNormal.wrapS= THREE.RepeatWrapping;
+iceNormal.wrapT= THREE.RepeatWrapping;
+iceNormal.repeat.x = 3;
+iceNormal.repeat.y = 2; 
+iceHeight.wrapS= THREE.RepeatWrapping;
+iceHeight.wrapT= THREE.RepeatWrapping;
+iceHeight.repeat.x = 3;
+iceHeight.repeat.y = 2;
 
 const metalPlatebasecolor = textureLoader.load("./Terracota_Tiles_003_basecolor.jpg");
 const metalPlatenormalMap = textureLoader.load("./Terracota_Tiles_003_normal.jpg");
@@ -60,31 +72,31 @@ const metalPlateheightMap = textureLoader.load("./Terracota_Tiles_003_height.png
 metalPlatebasecolor.wrapS= THREE.RepeatWrapping;
 metalPlatebasecolor.wrapT= THREE.RepeatWrapping;
 metalPlatebasecolor.repeat.x = 3;
-metalPlatebasecolor.repeat.y = 2; 
+metalPlatebasecolor.repeat.y = 1; 
 metalPlatenormalMap.wrapS= THREE.RepeatWrapping;
 metalPlatenormalMap.wrapT= THREE.RepeatWrapping;
 metalPlatenormalMap.repeat.x = 3;
-metalPlatenormalMap.repeat.y = 2; 
+metalPlatenormalMap.repeat.y = 1; 
 metalPlateheightMap.wrapS= THREE.RepeatWrapping;
 metalPlateheightMap.wrapT= THREE.RepeatWrapping;
 metalPlateheightMap.repeat.x = 3;
-metalPlateheightMap.repeat.y = 2; 
+metalPlateheightMap.repeat.y = 1; 
 
-const rocksHexagonbasecolor = textureLoader.load("./Ground_Dirt_008_basecolor.jpg");
-const rocksHexagonnormalMap = textureLoader.load("./Ground_Dirt_008_normal.jpg");
-const rocksHexagonheightMap = textureLoader.load("./Ground_Dirt_008_height.png");
-rocksHexagonbasecolor.wrapS= THREE.RepeatWrapping;
-rocksHexagonbasecolor.wrapT= THREE.RepeatWrapping;
-rocksHexagonbasecolor.repeat.x = 3;
-rocksHexagonbasecolor.repeat.y = 2; 
-rocksHexagonnormalMap.wrapS= THREE.RepeatWrapping;
-rocksHexagonnormalMap.wrapT= THREE.RepeatWrapping;
-rocksHexagonnormalMap.repeat.x = 3;
-rocksHexagonnormalMap.repeat.y = 2; 
-rocksHexagonheightMap.wrapS= THREE.RepeatWrapping;
-rocksHexagonheightMap.wrapT= THREE.RepeatWrapping;
-rocksHexagonheightMap.repeat.x = 3;
-rocksHexagonheightMap.repeat.y = 2      ; 
+const dirtBase = textureLoader.load("./Ground_Dirt_008_basecolor.jpg");
+const dirtNormal = textureLoader.load("./Ground_Dirt_008_normal.jpg");
+const dirtHeight = textureLoader.load("./Ground_Dirt_008_height.png");
+dirtBase.wrapS= THREE.RepeatWrapping;
+dirtBase.wrapT= THREE.RepeatWrapping;
+dirtBase.repeat.x = 7;
+dirtBase.repeat.y = 2; 
+dirtNormal.wrapS= THREE.RepeatWrapping;
+dirtNormal.wrapT= THREE.RepeatWrapping;
+dirtNormal.repeat.x = 7;
+dirtNormal.repeat.y = 2; 
+dirtHeight.wrapS= THREE.RepeatWrapping;
+dirtHeight.wrapT= THREE.RepeatWrapping;
+dirtHeight.repeat.x = 7;
+dirtHeight.repeat.y = 2      ; 
 
 const lavabasecolor = textureLoader.load("./LAVA_004_COLOR.jpg");
 const lavanormalMap = textureLoader.load("./LAVA_004_NORM.jpg");
@@ -132,7 +144,7 @@ sphere1.position.z = 3
 sphere1.add(cubeCamera)
 scene.add(sphere1)
 
-const sphere2 = new THREE.Mesh(new THREE.SphereGeometry(1, 512, 512), new THREE.MeshStandardMaterial({ map: stylizedBirckbasecolor, normalMap: stylizedBircknormalMap, displacementMap: stylizedBirckheightMap, displacementScale: 0.05} ))
+const sphere2 = new THREE.Mesh(new THREE.SphereGeometry(1, 512,512), new THREE.MeshStandardMaterial({ map: iceBase, normalMap: iceNormal, displacementMap: iceHeight, displacementScale: 0.05} ))
 sphere2.geometry.attributes.uv2 = sphere2.geometry.attributes.uv
 sphere2.position.y = 3
 sphere2.position.x = -4
@@ -140,7 +152,7 @@ sphere2.position.z = 6
 // sphere2.add(cubeCamera)
 scene.add(sphere2)
 
-const sphere3 = new THREE.Mesh(new THREE.SphereGeometry(1, 512, 512), new THREE.MeshStandardMaterial({ map: metalPlatebasecolor, normalMap: metalPlatenormalMap, displacementMap: metalPlateheightMap, displacementScale: 0.08}))
+const sphere3 = new THREE.Mesh(new THREE.TorusGeometry(1,0.4, 512, 512), new THREE.MeshStandardMaterial({ map: metalPlatebasecolor, normalMap: metalPlatenormalMap, displacementMap: metalPlateheightMap, displacementScale: 0.08}))
 sphere3.geometry.attributes.uv2 = sphere3.geometry.attributes.uv
 sphere3.position.y = 3
 sphere3.position.x = 0
@@ -148,7 +160,7 @@ sphere3.position.z = 6
 // sphere3.add(cubeCamera)
 scene.add(sphere3)
 
-const sphere4 = new THREE.Mesh(new THREE.SphereGeometry(1, 512, 512), new THREE.MeshStandardMaterial({ map: rocksHexagonbasecolor, normalMap: rocksHexagonnormalMap, displacementMap: rocksHexagonheightMap, displacementScale: 0.075}))
+const sphere4 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 512, 512), new THREE.MeshStandardMaterial({ map: dirtBase, normalMap: dirtNormal, displacementMap: dirtHeight, displacementScale: 0.075}))
 sphere4.geometry.attributes.uv2 = sphere4.geometry.attributes.uv
 sphere4.position.y = 3
 sphere4.position.x = -4
@@ -188,6 +200,7 @@ function animate() {
     sphere3.rotateY(0.005);
     sphere4.rotateX(-0.001);
     sphere4.rotateY(-0.005);
+    sphere4.rotateZ(0.000)
     sphere5.rotateX(0.001);
     sphere5.rotateY(0.005);
     controls.update();
